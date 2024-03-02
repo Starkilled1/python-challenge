8eb4bed125c533ec9cf5a87deedef7aad81a3af7
Javier Alarcon <javieralarconbusiness@outlook.com>
2024-03-02 16:28:23 -0700

    Added more code to the main section

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ import os
 election_csv = os.path.join( "Resources", "election_data.csv")
 
 candidates=[]
+candidates_names=[]
 
 
 # this section open the csv file and read the data and store into lists
@@ -14,9 +15,23 @@ with open(election_csv) as csvfile:
     for row in csvreader: 
         
         candidates.append(row[2])
+        
 
 # because we only need the information and not the head column of the csv we pop the first
 # entry of the list
 
 candidates.pop(0)
-print(len(candidates))
+
+# in order to know how many candidates are and what their name is we need to sort them 
+# in alphabetical order and removing the duplicates of the list with set in a for loop
+
+for name in sorted(set(candidates)):
+    
+    candidates_names.append(name)
+    count_per_cand = candidates.count(name)
+    
+
+print("Election Results") 
+print("-------------------------")
+print("Total Votes: " + str(len(candidates)))    
+print("-------------------------")
